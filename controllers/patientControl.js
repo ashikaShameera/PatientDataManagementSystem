7ecce7b420a22548d9ae038bc3a0b5033fd25900
Jayawinath Induwara <induwara43@gmail.com>
2023-10-05 23:58:23 +0530

    admin_patient update delete added,reg errors fixed

--- controllers/patientControl.js
+++ controllers/patientControl.js
@@ -17,14 +17,9 @@ module.exports.renderRegisterForm=(req,res)=>{
 }
 
 module.exports.createPatient=async (req,res)=>{
-    //creating newPatinet
-    // const patient=new Patient(req.body.patient);
-    // await patient.save();
-    //res.redirect(`/patient/${patient._id}`)
+   
     try {
         const patient = new Patient(req.body.patient);
-        
-        // Generate a salt and hash the password
         const saltRounds = 10; // Number of rounds (adjust as needed)
         const hashedPassword = await bcrypt.hash(patient.password, saltRounds);
         patient.password = hashedPassword
@@ -59,6 +54,41 @@ module.exports.createPatient=async (req,res)=>{
         }
 }
 
+module.exports.adminRegisterPatient=async(req,res)=>{
+    try {
+        const patient = new Patient(req.body.patient);
+        
+        // Generate a salt and hash the password
+        const saltRounds = 10; // Number of rounds (adjust as needed)
+        const hashedPassword = await bcrypt.hash(patient.password, saltRounds);
+        patient.password = hashedPassword
+           // Create a new User with the hashed password and role 'Patient'
+    const user = new User({
+        email: patient.email,
+        password: hashedPassword,
+        role: "Patient",
+        profile: patient._id
+      });
+  
+      // Save the User to the database
+      await user.save();
+      await patient.save();
+          res.redirect(`/admin/patient`);
+        } catch (error) {
+            if (error.code === 11000) {
+                // This error code (11000) indicates a duplicate key error
+                // Handle the duplicate email or NIC error here
+                res.status(400).render('admin/patient', {patients:null,
+                    error: 'Email or NIC is already in use. Please choose a different one.',
+                });
+            } else {
+                // Handle other errors (e.g., validation errors)
+                console.error('Error during registration:', error);
+                res.status(500).render('error', { error: 'Registration failed' });
+            }
+        }   
+   }
+  
 
 module.exports.showPatient=async(req,res)=>{
     const id=req.params.id;                     //Getting the patient id
@@ -67,11 +97,11 @@ module.exports.showPatient=async(req,res)=>{
 
     if(encodedDoctor){
         const doctor = JSON.parse(decodeURIComponent(encodedDoctor));   //Put doctor to JSON object
-        res.render('patient/show',{patient,doctor});//In here parse the whole doctor object to front it can reduce it
+        res.render('patient/show',{patient,doctor,error:null});//In here parse the whole doctor object to front it can reduce it
     }
     else{
         let doctor;         //To send null object of doctor if not it gives ejs error
-        res.render('patient/show',{patient,doctor});  //Internal Medicine
+        res.render('patient/show',{patient,doctor,error:null});  //Internal Medicine
     }
                  
 }
